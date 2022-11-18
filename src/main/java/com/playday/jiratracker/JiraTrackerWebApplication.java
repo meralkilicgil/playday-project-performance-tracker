92652d77ef2b3dceb1d37fe52d5008b049ec6a4c
meralkilicgil <mkilicgil@cyangate.com>
2022-11-18 15:56:02 +0300

    initial commit

---
+++ src/main/java/com/playday/jiratracker/JiraTrackerWebApplication.java
@@ -0,0 +1,13 @@
+package com.playday.jiratracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTrackerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraTrackerWebApplication.class, args);
+	}
+
+}
